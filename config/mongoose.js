33d614b1cbbe19cd39a8f44adc2f782a93fc694a
sunil <sunilkumarbajpai.skb@gmail.com>
2020-07-13 14:45:06 +0530

    completed

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/hospital-api-db", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 
 const db = mongoose.connection;
--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,3 @@
 module.exports.home=function(req,res){
-    return res.end('<h1>This is the API project only uses specified URLs, and not have UI.</h1>')
+    return res.end('<h1>This is the API project only uses specified URLs, and not have UI.</h1><br><h3> Read <a href="https://github.com/sunilkrbajpai/Hospital-API">Documentation</a> to get started.</h3>')
 }
